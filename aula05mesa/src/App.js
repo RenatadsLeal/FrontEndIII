8c93b43fd97dc98041a251139d487009b2db27e9
Renata Leal <85504267+RenatadsLeal@users.noreply.github.com>
2021-11-03 01:39:47 -0300

    update 5

---
+++ aula05mesa/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+import Banner from './components/Banner';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+function App() {
+  const empresa = {
+    nome: "Croissant. company",
+    cnpj: "6546545465"
+  }
+  return (
+    <div className="App">
+      <Header />
+      <Banner>Croissant</Banner>
+      <Footer empresa={empresa}></Footer>
+    </div>
+  );
+}
+
+export default App;
