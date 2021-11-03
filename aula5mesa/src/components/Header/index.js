0b770f0cda661ee281ec75319484e5d784485aae
Renata Leal <85504267+RenatadsLeal@users.noreply.github.com>
2021-11-03 13:46:49 -0300

    Helenaaaa

--- aula5mesa/src/components/Header/index.js
+++ aula5mesa/src/components/Header/index.js
@@ -2,18 +2,16 @@ import "./style.css"
 
 const Header = () => {
     return (
-        <header>
-            <div id="header">
+        <header className="header">
                 <div id="title"><h1>Croissant.</h1></div>
                 <div id="nav">
                     <nav>
                         <ul>
-                            <li><a href="#">Croissant? </a></li>
-                            <li><a href="#">More croissant </a></li>
+                            <li><a href="#">Croissant?</a></li>
+                            <li><a href="#">More croissant</a></li>
                         </ul>
                     </nav>
                 </div>
-            </div>
         </header>
 
     )
