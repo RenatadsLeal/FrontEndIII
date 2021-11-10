69880cbb7606bbf6d3909910f08b85d52e47e96e
Renata Leal <85504267+RenatadsLeal@users.noreply.github.com>
2021-11-10 01:50:06 -0300

    update classes

--- aula6/src/container/App.js
+++ aula6/src/container/App.js
@@ -1,19 +1,64 @@
 import '../styles/App.css';
+import { Fragment } from 'react';
 
 import ClassComponent from '../components/ClassComponent';
 import FunctionComponent from '../components/FunctionComponent';
  
 function App() {
+
+  let convidados = [
+    {
+      id: 0,
+      nome: "Carol",
+      estaNaLista: true,
+      tarefa: "brigadeiro"
+    },
+    {
+      id: 5,
+      nome: "Clarissa",
+      estaNaLista: true,
+      tarefa: "bolinho de queijo"
+    },
+    {
+      id: 12,
+    nome: "Helena",
+    estaNaLista: false,
+    tarefa: "Coca-cola"
+    }
+  ];
+
  return (
  <div className="App">
-  <h3>Convidado:</h3>
-  <ClassComponent nome="Nicolas" estaNaLista={true} />
+     <h3>Convidado:</h3>
+   {
+     convidados.map((convidado) => {
+       return (
+         <Fragment key={convidado.id}>
+           <ClassComponent nome = {convidado.nome} estaNaLista = {convidado.estaNaLista}/>
+         </Fragment>
+       )
+     })
+    }
+    <h3> Tarefas </h3>
+     {
+     convidados.map((convidado) => {
+      return (
+        <Fragment key={convidado.id}>
+          <FunctionComponent nome = {convidado.nome} tarefa={convidado.tarefa}/>
+          </Fragment>
+      )
+     })
+    }
+   
+  
+
+  {/* <ClassComponent nome="Nicolas" estaNaLista={true} />
   <ClassComponent nome="Pedro" estaNaLista={false} />
-  <ClassComponent nome="Carolina" estaNaLista={true} />
-  <h3>Tarefas: </h3>
-  <FunctionComponent nome="Nicolas" tarefa="batata-frita" />
+  <ClassComponent nome="Carolina" estaNaLista={true} /> */}
+
+  {/* <FunctionComponent nome="Nicolas" tarefa="batata-frita" />
   <FunctionComponent nome="Pedro" tarefa="pizza" />
-  <FunctionComponent nome="Carolina" tarefa="bebidas" />
+  <FunctionComponent nome="Carolina" tarefa="bebidas" /> */}
 </div>
 
  );
---
+++ aula8mesa/src/App.js
@@ -0,0 +1,13 @@
+import { Component } from 'react';
+import ListaMercado from './components/ListaMercado';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        <ListaMercado />
+      </>
+    )
+  }
+}
